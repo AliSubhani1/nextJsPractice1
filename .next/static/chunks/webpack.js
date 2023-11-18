3f5147e3a2b8375ef84a5c7ef07f93dc00a39bb7
tintash.ali.subhani <tintash.ali.subhani@crusoeenergy.com>
2023-11-18 15:07:03 +0500

    Create a main route and subroute using next js new app router.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4691b9b264a24906"; }
+/******/ 		__webpack_require__.h = function() { return "0a1270412c4f41c8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
